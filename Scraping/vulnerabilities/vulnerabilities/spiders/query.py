1aed6cc6be9b4dd901fbc5ad6eb7b27b47639023
CP-Tadeo <68222022+CP-Tadeo@users.noreply.github.com>
2023-11-04 17:20:20 +0800

    Initial commit

--- Scraping/vulnerabilities/vulnerabilities/spiders/query.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-connection = sqlite3.connect("vuln.db")
-cursor = connection.cursor()
-connection.commit()
-select_query = 'SELECT * from vulns'
-for result in cursor.execute(select_query):
-    print(result)
